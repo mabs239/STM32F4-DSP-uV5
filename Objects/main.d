106845e2ec10027fd223c22a54b7c2f854db7b7f
mabs239 <mabs239@gmail.com>
2015-09-10 14:15:13 +0500

    DSP Complex FFT working

--- Objects/main.d
+++ Objects/main.d
@@ -9,3 +9,9 @@
 .\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.4.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
 .\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
 .\objects\main.o: HelperFunctions.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.3.0\CMSIS\Include\arm_math.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.3.0\CMSIS\Include\core_cm4.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.3.0\CMSIS\Include\arm_const_structs.h
+.\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.3.0\CMSIS\Include\arm_common_tables.h
--- main.c
+++ main.c
@@ -7,61 +7,84 @@
 #include "stm32f4xx.h"                  // Device header
 #include <stdio.h>
 #include "HelperFunctions.h"
-
-void SYSCLK168(void) {
-	RCC->CFGR    =0x00000000;	
-	RCC->CR     &=~0x01000000;               // PLLON=0 to turn off PLL;	
-	RCC->PLLCFGR =0x24002A08;                // PLLN=168,PLLM=8,PLLP=00(/2),PLLSRC=0(HSI=f_In=16 MHz), f_VCO=336 MHz , f_Out=168 MHz;
-	RCC->CFGR    =0x00000002;                // SYSCLK=2(PLL) , AHB=APBx=SYSCLK (i.e. no preScalers at all);
-	RCC->CR     |=0x01000000;                // PLLON=1 to turn on PLL;
-	while(!(RCC->CR & 0x02000000)) {}        // Wait until PLLRDY=1;
-	while((RCC->CFGR & 0x0000000F)!=0x0A){}  // Wait until SYSCLK=PLL=168 MHz;
-}
-void SYSCLK16(void) {
-   // Reset the RCC clock configuration to the default reset state
-  /* Set HSION bit */
-  RCC->CR |= (uint32_t)0x00000001;
-  /* Reset CFGR register */
-  RCC->CFGR = 0x00000000;
-  /* Reset HSEON, CSSON and PLLON bits */
-  RCC->CR &= (uint32_t)0xFEF6FFFF;
-  /* Reset PLLCFGR register */
-  RCC->PLLCFGR = 0x24003010;
-  /* Reset HSEBYP bit */
-  RCC->CR &= (uint32_t)0xFFFBFFFF;
-	while((RCC->CFGR & 0x03)) {}        // Wait until SYSCLK=HSI=16 MHz;	
-}
-	
+#include <math.h>
+#define ARM_MATH_CM4
+#include "arm_math.h"
 
 /*
-void Blinky(void const *argument){
-	while(1){
-		GPIOD->BSRRL = (1 << 13);
-		osDelay(250);
-		GPIOD->BSRRH = (1 << 13);
-		osDelay(250);
-		printf("Allah");
-	}
-}
-*/
-	//osThreadDef(Blinky, osPriorityNormal, 1, 0);
+//DSP dot product
+float srcA_buf_f32[3] ={-4, -6, 1,};
+float srcB_buf_f32[3] ={7, 1, 3,};
+float32_t multOutput[3]; 
+arm_status status;  
 
-/*
- * main: initialize and start the system
- */
 int main (void) {
   double X[] = {.7,0.7};
 	svmPredict(X);
+	arm_mult_f32(srcA_buf_f32, srcB_buf_f32, multOutput, 3);
+	printf("%f, %f, %f", multOutput[0],multOutput[1],multOutput[2]);
+}
+*/
+
+
+//#define PI 3.14159265358979323846
+#include "arm_math.h"
+#include "arm_const_structs.h"
+#define TEST_LENGTH_SAMPLES 2048
+/* -------------------------------------------------------------------
+* External Input and Output buffer Declarations for FFT Bin Example
+* ------------------------------------------------------------------- */
+//extern float32_t testInput_f32_10khz[TEST_LENGTH_SAMPLES];
+//static float32_t testOutput[TEST_LENGTH_SAMPLES/2];
+
+float32_t testInput_f32_10khz[TEST_LENGTH_SAMPLES];
+static float32_t testOutput[TEST_LENGTH_SAMPLES/2];
+
+/* ------------------------------------------------------------------
+* Global variables for FFT Bin Example
+* ------------------------------------------------------------------- */
+uint32_t fftSize = 1024;
+uint32_t ifftFlag = 0;
+uint32_t doBitReverse = 1;
+uint32_t refIndex = 213, testIndex = 0;
+
+int32_t main(void)
+{
+  arm_status status;
+	float32_t maxValue;
+	int idx;
+	double angle;
+	for(idx = 0; idx < TEST_LENGTH_SAMPLES; idx++){ //Test Input
+		angle = (double)idx/TEST_LENGTH_SAMPLES * 2*PI;
+		testInput_f32_10khz[idx] = arm_sin_f32 (angle*10);
+		//printf("\n i=%i,val= %f   ", idx, testInput_f32_10khz[idx] );
+	}
 	
 	
-	//osKernelInitialize ();                // initialize CMSIS-RTOS
-	//RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;	// enable the clock to GPIOD
-	//GPIOD->MODER |= GPIO_MODER_MODER13_0;
+  status = ARM_MATH_SUCCESS;
+  arm_cfft_f32(&arm_cfft_sR_f32_len1024, testInput_f32_10khz, ifftFlag, doBitReverse);  /* Process the data through the CFFT/CIFFT module */
+  arm_cmplx_mag_f32(testInput_f32_10khz, testOutput, fftSize);/* Process the data through the Complex Magnitude Module for calculating the magnitude at each bin */
+  arm_max_f32(testOutput, fftSize, &maxValue, &testIndex);/* Calculates maxValue and returns corresponding BIN value */
+	
+	for(idx = 0; idx < 1023; idx++){
+		printf("\n %i %f",idx,testOutput[idx]);
+	}
+	
+	printf("\n Status = %i, MaxIndex = %i, MaxVal=%f",status, testIndex,maxValue);
+	
+  if(testIndex !=  refIndex)
+  {
+    status = ARM_MATH_TEST_FAILURE;
+  }
+	
 
-  // initialize peripherals here
-
-  // create 'thread' functions that start executing,
-  // example: tid_name = osThreadCreate (osThread(name), NULL);
-	//osThreadCreate(osThread (Blinky), NULL);
-	//osKernelStart ();                         // start thread execution 
+  /* ----------------------------------------------------------------------
+  ** Loop here if the signals fail the PASS check.
+  ** This denotes a test failure
+  ** ------------------------------------------------------------------- */
+  if( status != ARM_MATH_SUCCESS)
+  {
+    while(1);
+  }
+  while(1);                             /* main function does not return */
 }

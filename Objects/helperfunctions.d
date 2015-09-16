a6e10a36e7c84d28b72af63f7fff00c3ed8ae8d1
mabs239 <mabs239@gmail.com>
2015-09-16 16:31:03 +0500

    New Project

--- Objects/helperfunctions.d
+++
@@ -1,4 +0,0 @@
-.\objects\helperfunctions.o: HelperFunctions.c
-.\objects\helperfunctions.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\objects\helperfunctions.o: HelperFunctions.h
-.\objects\helperfunctions.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
